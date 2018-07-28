9d0ad3752e699bb13758a421fbc823939225456e
LAPTOP\Fergus <fergusfrl@gmail.com>
2018-07-28 13:00:08 +1200

    data migration tool for RiverServer

--- migrator.py
+++ migrator.py
@@ -1,6 +1,20 @@
 import csv
+import pymongo
+import re
 
-def readFromCSV(filePath):
+def connectToDB(uri):
+    client = pymongo.MongoClient(uri)
+    db = client.get_default_database()
+    guides = db['riverguides']
+
+    dbObject = readFromCSV()
+
+    guides.insert_many(dbObject)
+    print('Completed migration')
+
+
+def readFromCSV():
+    filePath = 'river-data.csv'
     data = []
 
     with open(filePath) as csvDataFile:
@@ -8,10 +22,21 @@ def readFromCSV(filePath):
         for row in csvReader:
             data.append(row)
         
-    return data[1:]
+    dbObject = map(lambda row: {
+        'title': row[3],
+        'river': row[2],
+        'region': row[1],
+        'grade': row[4],
+        'description': re.sub('<.*?>', '', row[11]),
+        'putInDescription': row[9],
+        'takeOutDescription': row[10]
+    }, data[1:])
+
+    return dbObject
 
 def main():
-    filePath = 'river-data-csv'
-    print(readFromCSV(filePath))
+    uri = <MongoDB URI>
+
+    connectToDB(uri)
 
 main()
