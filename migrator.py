963cea38a1be8d0f3d2d950edc13b5f6617ca11d
Fergus Farrell <fergusfrl@gmail.com>
2018-11-06 14:50:55 +1300

    Made migrator more generic

--- migrator.py
+++ migrator.py
@@ -1,27 +1,28 @@
 import csv
+import sys
 import pymongo
 import re
 
-def connectToDB(uri):
+
+# Connects to DB specified by uri and adds dbObject to the specified collection
+def migrateDataToDB(uri, collection, dbObject):
     client = pymongo.MongoClient(uri)
     db = client.get_default_database()
-    guides = db['riverguides']
-
-    dbObject = readFromCSV()
+    guides = db[collection]
 
     guides.insert_many(dbObject)
     print('Completed migration')
 
 
-def readFromCSV():
-    filePath = 'river-data.csv'
+# Asigns CSV values to a dbObject
+def readFromCSV(filePath):
     data = []
-
     with open(filePath) as csvDataFile:
         csvReader = csv.reader(csvDataFile)
         for row in csvReader:
             data.append(row)
-        
+    
+    # modify dbObject to work with particular CSV
     dbObject = map(lambda row: {
         'title': row[3],
         'river': row[2],
@@ -35,8 +36,13 @@ def readFromCSV():
     return dbObject
 
 def main():
-    uri = <MongoDB URI>
-
-    connectToDB(uri)
+    # there is no argument validation so get it right
+    uri = sys.argv[0]
+    collection = sys.argv[1]
+    CSVFilePath = sys.argv[2]
+    
+    dbObject = readFromCSV(CSVFilePath)
+    
+    migrateDataToDB(uri, collection, dbObject)
 
 main()
